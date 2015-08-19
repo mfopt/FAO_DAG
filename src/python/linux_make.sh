50734a9f20eca725d2adb1d6d81143190477e68f
steven <stevend2@stanford.edu>
2015-08-19 16:51:29 -0700

    added linux make

---
+++ src/python/linux_make.sh
@@ -0,0 +1,37 @@
+cd ../../include/pogs_fork/src/
+make gpu
+cd ../../scs
+make
+cd ../../
+cd src/python
+
+swig -python -c++ -I../ -outcurrentdir -o FAO_DAG_wrap.cu FAO_DAG.i
+
+nvcc -Xcompiler -fno-strict-aliasing,-fno-common,-dynamic,-fwrapv,-Wall,-Wstrict-prototypes \
+-I/usr/local/include -I/usr/local/opt/sqlite/include \
+-DNDEBUG -g -O3  -DLAPACK_LIB_FOUND \
+-I/usr/local/lib/python2.7/dist-packages/numpy/core/include \
+-I../ -I../../include/ -I../../include/scs/include/ \
+-I../../include/pogs_fork/src/include/ \
+-I/usr/include/python2.7 \
+-I/usr/local/include/python2.7 \
+-c FAO_DAG_wrap.cu \
+-o FAO_DAG_wrap.o \
+-std=c++11 -DFAO_GPU
+
+g++ -bundle -undefined dynamic_lookup \
+-L/usr/local/lib \
+-arch x86_64 -L/usr/local/include \
+-I/usr/local/include \
+FAO_DAG_wrap.o ../../include/pogs_fork/src/build/pogs.a \
+-L ../../include/scs/out -lscsindir \
+-L/usr/local/cuda/lib -L/usr/local/lib \
+-lcudart -lcublas -lcusparse -lcufftw \
+-L/usr/lib -L/usr/lib \
+-L/usr/lib \
+-lfftw3 -lfftw3f -lfftw3l -lfftw3_threads \
+-lfftw3f_threads -lfftw3l_threads \
+-o _FAO_DAG.so
+# echo ' ' >> FAO_DAG.i
+# python setup.py install
+# python ../../test/test_pogs_mat_free.py
